3b828caabbf6fd4cee2db5f10d536926ed2aec53
Kimberly Hayes <kshayes78@gmail.com>
2021-12-23 17:29:18 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+let number = 10;
+function add3() {
+    return (number += 3);
+}
+add3();
+function subtract2() {
+    return (number -= 2);
+}
+subtract2();
+ function multiply5() {
+     return (number *= 5);
+ }
+multiply5;
+function divideBy2() {
+  return (number /= 2);
+}
+divideBy2();
+
+function add(a, b) {
+    return (a + b);
+}
+ function subtract(a, b) {
+     return (a - b);
+ }
+function multiply(a, b){
+    return (a * b);
+}
+function divide(a, b){
+    return (a / b);
+}
+function increment(n){
+    return (++n);
+}
+ increment(n);
+function decrement(n){
+    return (--n);
+}
+ increment(n);
+ function makeInt(n){
+     return parseInt(n, 10);
+ }
+ makeInt(n);
+ function preserveDecimal(n) {
+     return parseFloat(n);
+ }
+ preserveDecimal;
